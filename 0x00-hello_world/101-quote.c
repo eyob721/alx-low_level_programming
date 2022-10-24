7a88a7af1156a9f700c1b1ef9dde04d2233ce6be
eyob721 <eyobmelkamu721@gmail.com>
2022-10-24 19:46:53 +0300

    Task 8 Advanced

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - a program that print a string to std error
+ *
+ * Return: 1 (in case of error)
+ */
+int main(void)
+{
+	dprintf(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
