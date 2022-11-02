e3ba9fa99d90c324deb4b56d2f212d023aeee759
eyob721 <eyobmelkamu721@gmail.com>
2022-11-02 13:42:31 +0300

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - changes the value of the integer it points to 98
+ * @n: an integer pointer
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -2,4 +2,7 @@
 
 ## Mandatory
 
+-   0-reset_to_98.c
+    -   a function that changes the value of an integer to 98, using an integer pointer.
+
 ## Advanced
