9bc6e46931087bdc079c03fba7ca9095aebc3dbf
eyob721 <eyobmelkamu721@gmail.com>
2022-11-14 19:59:02 +0300

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the product of two numbers
+ * @argc: argument count
+ * @argv: argument vector(array of string pointers)
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -8,5 +8,7 @@
     -   a program that prints the number of arguments passed to it.
 -   2-args.c
     -   a program that prints all the arguments passed to it.
+-   3-mul.c
+    -   a program that prints the product of two numbers.
 
 ## Advanced
