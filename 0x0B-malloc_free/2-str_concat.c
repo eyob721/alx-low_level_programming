b162bd9701dd9fd4ee45b82432eb0c21eb7aa702
eyob721 <eyobmelkamu721@gmail.com>
2022-11-17 11:01:47 +0300

    Task 2 fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,6 +13,11 @@ char *str_concat(char *s1, char *s2)
 	int i, j, size = 1; /* size = 1, to include the '\0' character */
 	char *con; /* concatenated new string */
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	for (i = 0; s1[i] != '\0'; i++)
 		size++;
 	for (i = 0; s2[i] != '\0'; i++)
