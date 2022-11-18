0f6f7125149654ac30f4aebd8fc3697740f03385
eyob721 <eyobmelkamu721@gmail.com>
2022-11-18 23:12:28 +0300

    Task 1 fix 3

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -25,10 +25,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		len2++;
 	/* Check if n is less than length of s2 */
 	/* And determine the length of the concatenated string */
-	if (n < len2)
-		size = len1 + n + 1;
-	else
-		size = len1 + len2 + 1;
+	size = len1 + len2 + 1;
 	/* Now allocate memory using the computed size */
 	str = malloc(size);
 	/* If memory allocation has failed, return NULL */
@@ -37,9 +34,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	/* Now that memory is allocated lets concatenate the two strings */
 	for (i = 0; s1[i] != '\0'; i++)
 		str[i] = s1[i];
-	for (j = 0; j < n && j < len2 + 1; j++, i++)
+	for (j = 0; j < n && s2[j] != '\0'; j++, i++)
 		str[i] = s2[j];
 	/* Dont forget to add teh null terminator */
+	str[i] = '\0';
 
 	return (str);
 }
