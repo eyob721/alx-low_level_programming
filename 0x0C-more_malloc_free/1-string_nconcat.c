fcf2c159f528c8ef4e168bc18e1e7f1031860d27
eyob721 <eyobmelkamu721@gmail.com>
2022-11-18 22:56:59 +0300

    Task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes
+ *
+ * Return: pointer to the concatented string, NULL (otherwise)
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j, len1 = 0, len2 = 0, size = 0;
+	char *str;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s1 = "";
+	/* Find the length of s1 */
+	for (i = 0; s1[i] != '\0'; i++)
+		len1++;
+	/* Find the length of s2 */
+	for (i = 0; s2[i] != '\0'; i++)
+		len2++;
+	/* Check if n is less than length of s2 */
+	/* And determine the length of the concatenated string */
+	if (n < len2)
+		size = len1 + n + 1;
+	else
+		size = len1 + len2 + 1;
+	/* Now allocate memory */
+	str = malloc(size);
+	/* If memory allocation has failed, return NULL */
+	if (str == NULL)
+		return (NULL);
+	/* Now that memory is allocated, lets concatenate the two strings */
+	for (i = 0; s1[i] != '\0'; i++)
+		str[i] = s1[i];
+	for (j = 0; j < n && s2[j] != '\0'; j++, i++)
+		str[i] = s2[j];
+	/* Dont forget to add teh null terminator */
+	str[i] = '\0';
+
+	return (str);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -4,5 +4,7 @@
 
 -   0-malloc_checked.c
     -   a function that allocates a memory using malloc. The function takes size of the memory as an argument and returns a void pointer to the allocated memory. If malloc fails it exits with exit status 98.
+-   1-string_nconcat.c
+    -   a function that concatenates two strings. The function takes as an argument two strings and number of bytes (n) as an integer. The function concatenates the first n bytes from the second string to the first string. If n is greater than the length of the second string, it concatenates the whole second string to the first string, it doesn't go beyond that.
 
 ## Advanced
